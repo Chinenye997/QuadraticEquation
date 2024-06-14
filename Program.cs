2d63feee4cc34fc470fcd76a87f2f702ca86c160
Gift <edehg997@gmail.com>
2024-06-14 15:13:41 +0100

    my project

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquationSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Quadratic Equation Solver");
+
+                // Ask the user for input
+                Console.Write("Enter coefficient a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter coefficient b: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter coefficient c: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                // Create a new QuadraticEquation object
+                QuadraticEquation equation = new QuadraticEquation(a, b, c);
+
+                // Calculate and display the roots
+                double root1 = equation.CalculateRoot1();
+                double root2 = equation.CalculateRoot2();
+
+                Console.WriteLine($"Root 1: {root1}");
+                Console.WriteLine($"Root 2: {root2}");
+                Console.ReadLine();
+        }
+    }
+}
---
+++ QuadraticEquation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquationSolver
+{
+    class QuadraticEquation
+    {
+            public double A { get; set; }
+            public double B { get; set; }
+            public double C { get; set; }
+
+            public QuadraticEquation(double a, double b, double c)
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+
+            public double CalculateDiscriminant()
+            {
+                return Math.Pow(B, 2) - 4 * A * C;
+            }
+
+            public double CalculateRoot1()
+            {
+                double discriminant = CalculateDiscriminant();
+                return (-B + Math.Sqrt(discriminant)) / (2 * A);
+            }
+
+            public double CalculateRoot2()
+            {
+                double discriminant = CalculateDiscriminant();
+                return (-B - Math.Sqrt(discriminant)) / (2 * A);
+            }
+        
+    }
+}
